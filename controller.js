8c4b34fb02b51c397ea3258b2772dd52e6c22370
harunkarakaya <karakayaharun67@gmail.com>
2023-08-07 02:38:24 +0300

    Define module and controller with js file

---
+++ controller.js
@@ -0,0 +1,9 @@
+app.controller("myController",function($scope){
+
+    $scope.name = "Harun";
+    $scope.surname = "KARAKAYA";
+
+}).controller("myController2", function($scope){
+
+    $scope.age=28;
+});
